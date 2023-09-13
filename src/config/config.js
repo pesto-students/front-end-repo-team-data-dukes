7db58e577432ea4049f7828d736acd2fa9a98f61
abhishek <asevarik@gmail.com>
2023-09-13 23:03:52 +0530

    changed to prod

--- src/config/config.js
+++ src/config/config.js
@@ -1,20 +1,20 @@
-// PRODUCTION URL
-// export const config = {
-//     url: "wss://api.talktimeapp.com",
-//     path: "/xmpp-websocket",
-//     unit_url: "talktimeapp.com",
-//     groupDomain: "conference.talktimeapp.com",
-//     groupKey: "@conference",
-//   };
-  
-  //DEVELOPEMENT CONFIG
-  export const config = {
+//PRODUCTION URL
+export const config = {
     url: "wss://api.talktimeapp.com",
     path: "/xmpp-websocket",
-    unit_url: "localhost",
-    groupDomain: "conference.localhost",
+    unit_url: "talktimeapp.com",
+    groupDomain: "conference.talktimeapp.com",
     groupKey: "@conference",
   };
+  
+  //DEVELOPEMENT CONFIG
+  // export const config = {
+  //   url: "wss://api.talktimeapp.com",
+  //   path: "/xmpp-websocket",
+  //   unit_url: "localhost",
+  //   groupDomain: "conference.localhost",
+  //   groupKey: "@conference",
+  // };
 
   //  url: "wss://kiranp33.dev-vc9.nouveau-labs.in",
   // path: "/r/nru78j/xmpp-websocket",
